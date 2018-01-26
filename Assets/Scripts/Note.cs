a83e5ac941fb64476841af8ce29b75fb7f2a2acc
Niall Cartwright <Carniall@googlemail.com>
2018-01-26 23:51:46 +0000

    Adding folder and new image

---
+++ Assets/Scripts/Note.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour {
+    public bool A;
+    public bool B;
+    public bool X;
+    public bool Y;
+
+    public bool Held;
+    [Range(0, 10f)]
+    public float HeldTime;
+
+    [SerializeField]
+    private GameObject A_GO;
+    [SerializeField]
+    private GameObject B_GO;
+    [SerializeField]
+    private GameObject X_GO;
+    [SerializeField]
+    private GameObject Y_GO;
+
+    private void Update()
+    {
+        A_GO.SetActive(A);
+        B_GO.SetActive(B);
+        X_GO.SetActive(X);
+        Y_GO.SetActive(Y);
+    }
+}
