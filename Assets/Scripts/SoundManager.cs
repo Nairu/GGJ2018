9e03feb57db6bce9c752cf5f028b4b9611ea07cc
Connor <connor.checkley@gmail.com>
2018-01-27 18:55:27 +0000

    Get notes mod

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -9,20 +9,25 @@ using System.Linq;
 public class SoundManager : MonoBehaviour
 {
     MidiFile f;
+    TempoMap tMap;
     AudioSource audioSource;
     // Use this for initialization
     void Start()
     {
         f = MidiFile.Read(Application.dataPath + "/Midi/" + "tetris_midi.mid");
+        tMap = f.GetTempoMap();
         audioSource = GetComponent<AudioSource>();
         if (audioSource.isPlaying)
             audioSource.Play();
+        //foreach (var v in f.GetNotes().AtTime(Convert.ToInt64(1)))
+        //    Debug.Log(v.NoteNumber);
     }
 
     // Update is called once per frame
     void Update()
     {
-        foreach(var v in f.GetNotes())
-        Debug.Log(v.NoteNumber);
+        //audioSource.GetSpectrumData(float[], 0, FFTWindow.BlackmanHarris);
+        foreach (var v in f.GetNotes().AtTime(new MetricTimeSpan(0, 0, Convert.ToInt32(audioSource.time + 1)), tMap, LengthedObjectPart.Entire))
+            Debug.Log(v.NoteNumber);
     }
 }
