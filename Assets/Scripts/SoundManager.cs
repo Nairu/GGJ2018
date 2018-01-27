150e0a7d075b8568b54fd9b249706ae7e09a9b81
Connor <connor.checkley@gmail.com>
2018-01-27 16:43:44 +0000

    Midi Integration

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Melanchall.DryWetMidi.Smf;
+
+public class SoundManager : MonoBehaviour
+{
+    AudioSource audioSource;
+    // Use this for initialization
+    void Start()
+    {
+        //var x = Resources.Load<MidiFile>("");
+        //var x = Resources.GetBuiltinResource<MidiFileFormat>("");
+        //MidiFile.Read(Resources.GetBuiltinResource)
+        MidiFile f = MidiFile.Read(Application.dataPath + "/Midi/" + "tetris_midi");
+        //audioSource = GetComponent<AudioSource>();
+        //if (audioSource.isPlaying)
+        //    audioSource.Play();
+        if (f.Chunks.Count > 0)
+        {
+            Debug.Log("We have a midi file loaded!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
