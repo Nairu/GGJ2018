1ed6b8391a60ab915326044a57bc4b641cca171f
Joe Taylor <CoderJet@gmail.com>
2018-01-28 02:45:09 +0000

    Check in asae pafce

--- Assets/Scripts/MenuNavigator.cs
+++ Assets/Scripts/MenuNavigator.cs
@@ -26,16 +26,9 @@ public class MenuNavigator : MonoBehaviour
         OI_BUTTON_X,
         OI_BUTTON_Y
     }
-    public Animator StartAnimator;
-    public Animator OptionsAnimator;
-    public Animator ExitAnimator;
     [Space]
     public GameObject MainMenuScreen;
     public GameObject OptionsScreen;
-    [Space]
-    public TextMeshProUGUI StartGameText;
-    public TextMeshProUGUI OptionsText;
-    public TextMeshProUGUI QuiteGameText;
     [Space]
     public float NavigationDelay = 0.5f;
 
@@ -59,93 +52,97 @@ public class MenuNavigator : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (navigationTimeOut >= NavigationDelay)
-        {
-            float inputY = Input.GetAxisRaw("Vertical");
+    //// Update is called once per frame
+    //void FixedUpdate()
+    //{
+    //    if (navigationTimeOut >= NavigationDelay)
+    //    {
+    //        float inputY = Input.GetAxisRaw("Vertical");
 
-            if (currMenu == Menu.M_MAIN_MENU)
-            {
-                if (Input.GetButton("Fire1"))
-                {
-                    switch (currMenuItem)
-                    {
-                        case MainMenuItem.MMI_START:
-                            // TODO: Load the game scene.
-                            Debug.Log("This is when the game would run.");
-                            break;
-                        case MainMenuItem.MMI_OPTIONS:
-                            MainMenuScreen.SetActive(false);
-                            OptionsScreen.SetActive(true);
-                            currMenu = Menu.M_OPTIONS_MENU;
-                            break;
-                        case MainMenuItem.MMI_EXIT:
-                            Application.Quit();
-                            break;
-                    }
-                }
-                else
-                {
-                    int currentIdx = (int)currMenuItem;
+    //        if (currMenu == Menu.M_MAIN_MENU)
+    //        {
+    //            if (Input.GetButton("Fire1"))
+    //            {
+    //                switch (currMenuItem)
+    //                {
+    //                    case MainMenuItem.MMI_START:
+    //                        // TODO: Load the game scene.
+    //                        Debug.Log("This is when the game would run.");
+    //                        break;
+    //                    case MainMenuItem.MMI_OPTIONS:
+    //                        MainMenuScreen.SetActive(false);
+    //                        OptionsScreen.SetActive(true);
+    //                        currMenu = Menu.M_OPTIONS_MENU;
+    //                        break;
+    //                    case MainMenuItem.MMI_EXIT:
+    //                        Application.Quit();
+    //                        break;
+    //                }
+    //            }
+    //            else
+    //            {
+    //                int currentIdx = (int)currMenuItem;
                     
-                    if (inputY == 1)
-                    {
-                        if (currentIdx == 0)
-                        {
-                            currentIdx = menuItemLength;
-                        }
-                        else
-                        {
-                            currentIdx = --currentIdx % menuItemLength;
-                        }
-                    }
-                    else if (inputY == -1)
-                    {
-                        currentIdx = ++currentIdx % menuItemLength;
-                    }
+    //                if (inputY == 1)
+    //                {
+    //                    if (currentIdx == 0)
+    //                    {
+    //                        currentIdx = menuItemLength;
+    //                    }
+    //                    else
+    //                    {
+    //                        currentIdx = --currentIdx % menuItemLength;
+    //                    }
+    //                }
+    //                else if (inputY == -1)
+    //                {
+    //                    currentIdx = ++currentIdx % menuItemLength;
+    //                }
                     
-                    if ((MainMenuItem)currentIdx != currMenuItem)
-                    {
-                        currMenuItem = (MainMenuItem)currentIdx;
+    //                if ((MainMenuItem)currentIdx != currMenuItem)
+    //                {
+    //                    currMenuItem = (MainMenuItem)currentIdx;
 
-                        StartAnimator.ResetTrigger("StartSelected");
-                        OptionsAnimator.ResetTrigger("OptionsSelected");
-                        ExitAnimator.ResetTrigger("ExitSelected");
+    //                    //StartAnimator.Play("StartHighlight");// .ResetTrigger("StartSelected");
+    //                    //OptionsAnimator.ResetTrigger("OptionsSelected");
+    //                    //ExitAnimator.ResetTrigger("ExitSelected");
 
-                        if (currMenuItem == MainMenuItem.MMI_START)
-                        {
-                            Debug.Log("Setting Start");
-                            StartAnimator.SetTrigger("StartSelected");                            
-                        }
-                        else if (currMenuItem == MainMenuItem.MMI_OPTIONS)
-                        {
-                            Debug.Log("Setting Options");
-                            OptionsAnimator.SetTrigger("OptionsSelected");                            
-                        }
-                        else
-                        {
-                            Debug.Log("Setting Exit");
-                            ExitAnimator.SetTrigger("ExitSelected");
-                        }
-                    }
-                }
-            }
-            else if (currMenu == Menu.M_OPTIONS_MENU)
-            {
-                int currentIdx = (int)currOptionItem;
+    //                    if (currMenuItem == MainMenuItem.MMI_START)
+    //                    {
+    //                        StartAnimator.Play("StartHighlight");
+    //                        Debug.Log("Setting Start");
+    //                        //StartAnimator.SetTrigger("StartSelected");                            
+    //                    }
+    //                    else if (currMenuItem == MainMenuItem.MMI_OPTIONS)
+    //                    {
+    //                        OptionsAnimator.Play("OptionHighlight");
+    //                        Debug.Log("Setting Options");
+    //                        //OptionsAnimator.SetTrigger("OptionsSelected");                            
+    //                    }
+    //                    else
+    //                    {
+    //                        ExitAnimator.Play("ExitHighlight");
+    //                        Debug.Log("Setting Exit");
+    //                        //ExitAnimator.SetTrigger("ExitSelected");
+    //                    }
+    //                }
+    //            }
+    //        }
+    //        else if (currMenu == Menu.M_OPTIONS_MENU)
+    //        {
+    //            int currentIdx = (int)currOptionItem;
 
-                if (inputY >= 1)
-                    currentIdx = --currentIdx % optionItemLength;
-                else if (inputY <= -1)
-                    currentIdx = ++currentIdx % optionItemLength;
+    //            if (inputY >= 1)
+    //                currentIdx = --currentIdx % optionItemLength;
+    //            else if (inputY <= -1)
+    //                currentIdx = ++currentIdx % optionItemLength;
 
-                if ((OptionsItem)currentIdx != currOptionItem)
-                    currOptionItem = (OptionsItem)currentIdx;
-            }
-            navigationTimeOut = 0f;
-        }
-        navigationTimeOut += Time.fixedDeltaTime;
-    }
+    //            if ((OptionsItem)currentIdx != currOptionItem)
+    //                currOptionItem = (OptionsItem)currentIdx;
+    //        }
+    //        navigationTimeOut = 0f;
+    //    }
+    //    navigationTimeOut += Time.fixedDeltaTime;
+    //}
+
 }
